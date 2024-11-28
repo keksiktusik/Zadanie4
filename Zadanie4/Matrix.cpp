9643ca099015cc93bea9601c2dbed61490fc7e92
Marta <martaprzyszowa@gmail.com>
2024-11-28 10:29:30 +0100

    matrix.cpp - Pomocnicze funkcje

---
+++ Zadanie4/Matrix.cpp
@@ -0,0 +1,21 @@
+#include "matrix.h"
+
+// Pomocnicze funkcje
+void matrix::freeMemory() {
+    if (data) {
+        for (int i = 0; i < n; ++i) {
+            delete[] data[i];
+        }
+        delete[] data;
+        data = nullptr;
+    }
+}
+
+void matrix::allocateMemory(int size) {
+    freeMemory();
+    n = size;
+    data = new int* [n];
+    for (int i = 0; i < n; ++i) {
+        data[i] = new int[n]();
+    }
+}
--- Zadanie4/Zadanie4.cpp
+++ Zadanie4/Zadanie4.cpp
@@ -1,10 +1,19 @@
-﻿// Zadanie4.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+﻿#include <iostream>
+#include "matrix.h"
+
+int main() {
+    int size = 4;
+    int table[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+
+    matrix m1(size, table);
+    std::cout << "Macierz m1:\n" << m1;
+
+    m1.dowroc();
+    std::cout << "\nMacierz transponowana m1:\n" << m1;
+
+    m1.losuj();
+    std::cout << "\nMacierz losowa m1:\n" << m1;
 
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    return 0;
 }
-
