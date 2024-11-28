19628d71a1d332d52d56d08d2e94fd3c67f8783b
Marta <martaprzyszowa@gmail.com>
2024-11-28 10:33:47 +0100

    Dodanie części konstruktorów

--- Zadanie4/Matrix.cpp
+++ Zadanie4/Matrix.cpp
@@ -19,3 +19,20 @@ void matrix::allocateMemory(int size) {
         data[i] = new int[n]();
     }
 }
+// Konstruktor domyœlny
+matrix::matrix() : n(0), data(nullptr) {}
+
+// Konstruktor z rozmiarem
+matrix::matrix(int n) : n(n), data(nullptr) {
+    allocateMemory(n);
+}
+
+// Konstruktor z tabel¹
+matrix::matrix(int n, int* t) : n(n), data(nullptr) {
+    allocateMemory(n);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            data[i][j] = t[i * n + j];
+        }
+    }
+}
