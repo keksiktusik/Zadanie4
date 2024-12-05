6039f3bc1392a78e7c998e0d610128d030479009
keksiktusik <163601121+keksiktusik@users.noreply.github.com>
2024-12-05 10:50:10 +0100

    przywrócenie usuniętego plikiu

--- Zadanie4/matrix.h
+++ Zadanie4/matrix.h
@@ -29,64 +29,6 @@
   */
 class matrix {
 private:
-<<<<<<< HEAD
-    /// Rozmiar macierzy n x n
-    int n;         
-    /// Dynamiczna alokacja danych macierzy
-    int** data;    
-    /// Pomocnicza metoda do zwalniania pamiêci
-    void freeMemory();  
-    /// Pomocnicza metoda do alokacji pamiêci
-    void allocateMemory(int size); 
-
-public:
-    /// Konstruktor domyœlny
-    matrix();                     
-    /// Konstruktor o wymiarach n x n
-    matrix(int n);               
-    /// Konstruktor z tabel¹ danych
-    matrix(int n, int* t);       
-    /// Konstruktor kopiuj¹cy
-    matrix(const matrix& m);       
-    /// Destruktor
-    ~matrix();                     
-
-    /// Alokacja pamiêci
-    matrix& alokuj(int n);         
-    /// Wstawianie wartoœci
-    matrix& wstaw(int x, int y, int wartosc); 
-    /// Pobieranie wartoœci
-    int pokaz(int x, int y);       
-    /// Transponowanie macierzy
-    matrix& dowroc();              
-    /// Losowe wype³nienie macierzy
-    matrix& losuj();              
-    /// Losowe wype³nienie czêœci macierzy
-    matrix& losuj(int x);         
-    /// Macierz diagonalna
-    matrix& diagonalna(int* t);    
-    /// Przek¹tna przesuniêta
-    matrix& diagonalna_k(int k, int* t); 
-
-    // Operatory
-    /// Dodawanie macierzy
-    matrix& operator+(matrix& m);  
-    /// Mno¿enie macierzy
-    matrix& operator*(matrix& m);  
-    /// Dodawanie liczby
-    matrix& operator+(int a);      
-    /// Mno¿enie przez liczbê
-    matrix& operator*(int a);      
-    /// Porównanie równoœci
-    bool operator==(const matrix& m) const; 
-    /// Porównanie wielkoœci
-    bool operator>(const matrix& m) const;  
-    /// Porównanie mniejszoœci
-    bool operator<(const matrix& m) const;  
-
-    /// Wypisanie macierzy
-    friend std::ostream& operator<<(std::ostream& os, const matrix& m); 
-=======
     int n;         ///< Rozmiar macierzy \c n x \c n.
     int** data;    ///< Dynamicznie alokowana tablica dwuwymiarowa przechowuj¹ca dane macierzy.
 
@@ -255,7 +197,6 @@ public:
      * @return Referencja do strumienia wyjœciowego.
      */
     friend std::ostream& operator<<(std::ostream& os, const matrix& m);
->>>>>>> fc2100b (uzupeÅ‚nienie komentarzy)
 };
 
 #endif
